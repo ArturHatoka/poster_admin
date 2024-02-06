9b22b6f9e5e878401d359baf2b27ab7281c2d5c5
artur.hatoka <artur.hatoka@yandex.ru>
2024-02-06 16:44:54 +0300

    db

--- database.js
+++ database.js
@@ -4,6 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize('postgres://postgres:123@localhost:5432/lucky'); // Используйте переменную окружения для URL базы данных
+const sequelize = new Sequelize(process.env.DB_CONNECT); // Используйте переменную окружения для URL базы данных
 
 export { sequelize};
